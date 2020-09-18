e9a57c8ea4129183d6dba60d25098013b84fa308
Jason Sugata <jasonsugata@gmail.com>
2020-09-17 21:30:29 -0400

    added dropdown user menu

--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -6,8 +6,18 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from "react-router-dom"
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
+import { Link } from "react-router-dom"
+import searchIcon from "./icons/search-bar-icon.png"
+import supportIcon from "./icons/get-support-icon.png"
+import menuIcon from "./icons/menu-icon.png"
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -22,6 +32,21 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
@@ -30,6 +55,9 @@ export default function ButtonAppBar() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
+          <img src={searchIcon} />
+          <img src={menuIcon} />
+
           <Link
             style={{ textDecoration: 'none', color: 'white' }}
             className="link" to='/home'>HOME </Link>
@@ -49,9 +77,56 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             DEMO!
           </Typography>
+          <Typography variant="h6" className={classes.title}>
+            <img src={supportIcon} />
+            Support
+            </Typography>
+
+
+          <div>
+
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <Typography variant="h6" className={classes.title}>
+                User
+                {!open ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+
+              </Typography>
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              getContentAnchorEl={null}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+
+
+
+
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
     </div>
   );
 }
